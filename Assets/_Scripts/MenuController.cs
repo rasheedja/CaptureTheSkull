53ba6d374ebfe1e04c2883ad367a31d41aface93
Junaid Rasheed <jrasheed178@gmail.com>
2018-04-14 02:02:25 +0100

    Complete Terrain setup and Main Menu

---
+++ Assets/_Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MenuController : MonoBehaviour
+{
+    public Transform cameraStartLookAt;
+    public Transform cameraEndLookAt;
+    public Transform cam;
+
+    void Start()
+    {
+        cam.LookAt(cameraStartLookAt);
+        cam.DOLookAt(cameraEndLookAt.position, 10).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    public void ButtonHandlerStartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void ButtonHandlerQuitGame()
+    {
+        Application.Quit();
+    }
+}
